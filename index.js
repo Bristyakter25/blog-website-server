7f3f95c8c2858d316c8e5544432694d1ab8a688b
Bristyakter25 <sabihaakterbristy@gmail.com>
2024-12-26 23:46:00 +0600

    added comment section

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+require('dotenv').config();
 const { ObjectId } = require("mongodb");
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
+
+// app.use(cors({
+//   origin: [
+//   'http://localhost:5178',
+//   'https://blog-website-8c111.web.app',
+//   'https://blog-website-8c111.firebaseapp.com'],
+ 
+// }));
 
 app.use(cors());
 app.use(express.json());
@@ -32,10 +41,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // blogs related apis
 
